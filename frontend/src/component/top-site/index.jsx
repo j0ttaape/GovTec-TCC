b4729f3dfcd79101fa13efe8e137278f6a4d5c44
j0ttaape <joaopedrotrajano59@gmail.com>
2025-10-28 08:05:53 -0300

    criando projeto

---
+++ frontend/src/component/top-site/index.jsx
@@ -0,0 +1,12 @@
+import Link from 'react-router-dom'
+
+
+export default function TopSite(){
+
+    return(
+        <section className="container-topsite">
+            <h2>GovTec</h2>
+            
+        </section>
+    )
+}
